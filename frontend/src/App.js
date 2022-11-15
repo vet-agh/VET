e33ae24e586161e526368f5a904c6cc653c10635
krawczk <kacperkrawczyk490@gmail.com>
2022-11-15 17:44:38 +0100

    VET-32 React home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './views/Home'
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
