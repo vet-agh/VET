4ae0b3f47ed87ad5724ddf72c256cad5cc6a36d6
krawczk <kacperkrawczyk490@gmail.com>
2022-11-15 16:15:28 +0100

    VET-32 Setting up express app

--- backend/server.js
+++ backend/server.js
@@ -8,3 +8,13 @@ const express = require('express')
 // Dotenv package
 
 require('dotenv').config()
+
+// Express app
+
+const app = express()
+
+// Listening for requests
+
+app.listen(process.env.PORT , () => {
+  console.log('Listening on port 4000')
+})
