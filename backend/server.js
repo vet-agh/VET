430e629358228b74f35b7563de04a24c03d78462
krawczk <kacperkrawczyk490@gmail.com>
2022-11-15 18:03:14 +0100

    VET-32 Database connection

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@
 
 const express = require('express')
 
+// Mongoose package
+
+const mongoose = require('mongoose')
+
 // Dotenv package
 
 require('dotenv').config()
@@ -19,8 +23,14 @@ app.use((req, res, next) => {
   next()
 })
 
-// Listening for requests
+// Connect to database and listen on port 4000
 
-app.listen(process.env.PORT , () => {
-  console.log('Listening on port 4000')
-})
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT , () => {
+      console.log('Connected to database and listening on port 4000')
+    })
+  }) 
+  .catch((error) => {
+    console.log(error) 
+  }) 
