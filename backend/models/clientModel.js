7781c3a13955063d74b461cf93ae6c1c22427125
ZuzannaKi <zkielbasa@student.agh.edu.pl>
2022-11-17 15:14:05 +0100

    VET-15 Client data model (Schema)

---
+++ backend/models/clientModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+//creating new Schema 
+const clinetSchema = new Schema({
+    imie:{
+        type: String,
+        required: true
+    },
+    nazwisko: {
+        type: String,
+        required : true
+    },
+    numer_konta:{
+        type: String
+    },
+    id_pacjenta:{
+        type: Number,
+        required: true
+    }
+},{timestamps:true}) //add data when doc were created
+module.exports = mongoose.model('Client',clinetSchema)
+
