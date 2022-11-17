25263543e23901d57ebff7e84f017b7741fc40d2
ZuzannaKi <zkielbasa@student.agh.edu.pl>
2022-11-17 16:49:09 +0100

    VET-15 CLIENTS ROUTE

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -1,35 +1,34 @@
 // Imports
+
 const {
     createClient,
+    getClient,
+    getClients,
+    deleteClient,
+    updateClinet
 
 } = require('../controllers/clientController')
 // Express package
 const express = require('express')
 
-// Employee model
-const Employee = require('../models/employeeModel')
+// Client model
+const Client = require('../models/clientModel')
 
 // Router object to handle routes for employees
 
 const router = express.Router()
 //GET all clients
-router.get('/',(req,res)=>{
-    res.json({mssg: 'GET all clients'})
-})
+router.get('/',getClients)
 //GET a single client
-router.get('/:id',(req,res)=>{
-    res.json({mssg: 'GET a single client'})
-})
+router.get('/:id',getClient)
 
 //POST new client
 router.post('/',createClient)
 
 //DELETE a client 
-router.delete('/:id', (req,res)=>{
-    res.json({mssg:'DELETE a client'})
-})
+router.delete('/:id', deleteClient)
 
 //UPDATE a client
-router.patch('/:id',(req,res)=>{
-    res.json({mssg: 'UPDATE a client'})
-})
+router.patch('/:id',updateClinet)
+
+module.exports = router
