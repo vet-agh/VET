4015b7cfa96234ce01816ddd746300f074f1c439
KacperCz <kacper.czarnik2000@gmail.com>
2022-11-17 22:16:12 +0100

    Patient Controller

---
+++ backend/patientController.js
@@ -0,0 +1,34 @@
+const Patient = require('../models/patientModel')
+
+
+const createPatient = async(req, res) => {
+    const {imie, gatunek, rasa, id_wlasciciela} = req.body
+
+    try {
+        const patient = await Patient.create({imie, gatunek, rasa, id_wlasciciela})
+        res.status(200).json(patient)
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
+
+    
+}
+
+const getPatients = async(req, res) => {
+    const patients = await Patient.find({}).sort({createdAt: -1})
+
+    res.status(200).json(patients)
+}
+
+const getPatient = async(req, res) => {
+    const{ id } = req.params
+
+
+    const patient = await Patient.findByID(id)
+
+    if(!patient) {
+        return res.status(404).json({error: 'No such patient'})
+    }
+
+    res.status(200).json(patient)
+}
