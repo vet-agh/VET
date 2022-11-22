cf590258d1f1a44bfa22d60c35ab2425c7a1eef2
krawczk <kacperkrawczyk490@gmail.com>
2022-11-22 12:32:50 +0100

    VET-22 Add function to delete employees

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -47,6 +47,22 @@ const addEmployee = async (req, res) => {
   }
 }
 
+const deleteEmployee = async(req, res) => {
+  const { id } = req.params
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({error: "No employee with given id."})
+  }
+
+  const employee = await Employee.findById({_id: id})
+
+  if(!employee) {
+    return res.status(404).json({error: "No employee with given id."})
+  }
+
+  res.status(200).json(employee)
+}
+
 // Exports
 
 module.exports = {
