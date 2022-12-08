19f01c27726c8c549cee4138b0193ef2a6a3eafd
KacperCz <kacper.czarnik2000@gmail.com>
2022-12-08 22:58:54 +0100

    uploading home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
 import Home from './views/Home'
+
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar/>
         <div className="pages">
           <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-            />
+            <Route path="/" element={<Home/>}/>
           </Routes>
         </div>
       </BrowserRouter>
--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,9 +1,32 @@
-const Home = () => {
+const Home = () => 
+{
   return (
-    <div className="home">
-      <h2>Home</h2>
+  <>
+    <center><h3> Strona startowa Sieci Weterynaryjnej</h3></center>
+    <br></br>
+    <p>Jesteś zalogowany jako administrator</p>
+    <div class="NavigationButtons">
+
+      <form action="/clients">
+      <input type="submit" value="Klienci"/></form>
+      
+      <form action="/patients">
+      <input type="submit" value="Pacjenci"/></form> 
+      
+      <form action="/schedules">
+      <input type="submit" value="Wizyty"/></form> 
+      
+      <form action="/equipment">
+      <input type="submit" value="Sprzęt"/></form> 
+      
+      <form action="/employees">
+      <input type="submit" value="Pracownicy"/></form> 
+      
+      <form action="/clinics">
+      <input type="submit" value="Zakłady"/></form> 
+
     </div>
+  </>
   )
 }
-
 export default Home
