9df2dcc8bd790bd8d83af4f8a4bfd945f4bbb0d7
krawczk <kacperkrawczyk490@gmail.com>
2022-12-10 02:10:03 +0100

    VET-68 Create css class for delete button in form

--- frontend/src/components/ScheduleDetails.js
+++ frontend/src/components/ScheduleDetails.js
@@ -16,7 +16,7 @@ const ScheduleDetails = ({schedule}) => {
 
   return (
     <div className="schedule-details">
-      <button onClick={handleClickDelete}>Usuń wizyte</button>
+      <button class="delete-button" onClick={handleClickDelete}>Usuń wizyte</button>
       <p><strong>Data:</strong> {schedule.data.substring(0, 10) + ' ' + schedule.data.substring(11, 19)}</p>
       <p><strong>ID Klniki</strong>: {schedule.id_kliniki}</p> 
       <p><strong>ID Lekarza</strong>: {schedule.id_lekarza}</p>
