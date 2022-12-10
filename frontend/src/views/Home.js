f476dadd29a7bddec8d3bfc46faf8fdae48cea97
krawczk <kacperkrawczyk490@gmail.com>
2022-12-10 01:49:34 +0100

    VET-62 Changing title rom Wizyty to Harmonogram

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -14,7 +14,7 @@ const Home = () =>
       <input type="submit" value="Pacjenci"/></form> 
       
       <form action="/schedule">
-      <input type="submit" value="Wizyty"/></form> 
+      <input type="submit" value="Harmonogram"/></form> 
       
       <form action="/equipment">
       <input type="submit" value="Sprzęt"/></form> 
