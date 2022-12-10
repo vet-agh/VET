cd6900568b3c128289d824cebe35383bbca1d60f
krawczk <kacperkrawczyk490@gmail.com>
2022-12-10 01:48:50 +0100

    VET-62 ScheduleDetails

--- frontend/src/components/ScheduleDetails.js
+++ frontend/src/components/ScheduleDetails.js
@@ -1,12 +1,15 @@
 const ScheduleDetails = ({schedule}) => {
   return (
     <div className="schedule-details">
-      <strong><p>Data: {schedule.data} { } ID Kliniki: {schedule.id_kliniki} ID Lekarza: {schedule.id_lekarza}</p></strong>
-      <p>ID Klienta: {schedule.id_klienta}</p>
-      <p>ID Pacjenta: {schedule.id_pacjenta}</p>
-      <p>Minimalny czas trwania: {schedule.czas_trwania_min}</p>
-      <p>Usługa: {schedule.usluga}</p>
-      <p>Data dodania wizyty do harmonogramu: {schedule.createdAt}</p>
+      <p><strong>Data:</strong> {schedule.data.substring(0, 10) + ' ' + schedule.data.substring(11, 19)}</p>
+      <p><strong>ID Klniki</strong>: {schedule.id_kliniki}</p> 
+      <p><strong>ID Lekarza</strong>: {schedule.id_lekarza}</p>
+      <p><strong>ID Klienta</strong>: {schedule.id_klienta}</p>
+      <p><strong>ID Pacjenta</strong>: {schedule.id_pacjenta}</p>
+      <p><strong>Minimalny czas trwania</strong>: {schedule.czas_trwania_min}</p>
+      <p><strong>Usługa</strong>: {schedule.usluga}</p>
+      <p><i>Data dodania wizyty do harmonogramu:</i> {schedule.createdAt.substring(0, 10)}</p>
+      <br></br>
     </div>
   )
 }
