8456dd83418398fb42b2bee35856b24f9f7c2c01
krawczk <kacperkrawczyk490@gmail.com>
2022-12-10 01:15:12 +0100

    VET-62 style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Importing context providers
+//Clients
+//Clinics
+//Equipment
+//Patients
+import {ScheduleContextProvider} from './context/ScheduleContext';
+//Employees
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ScheduleContextProvider>
+      <App />
+    </ScheduleContextProvider>
   </React.StrictMode>
 );
