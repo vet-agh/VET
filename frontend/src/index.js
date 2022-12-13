ad5a41bc38665e61cce375a5b8a9e656b1e0ab04
Michał Sawicki <msawicki@student.agh.edu.pl>
2022-12-13 17:14:36 +0100

    VET-60 small changes in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+// Importing context providers
+
+
 import { EquipmentContextProvider } from './context/EquipmentContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
