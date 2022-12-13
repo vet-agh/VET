ddb5a033fdac1aa2f860457e95c3d9b72a754ea1
ZuzannaKi <zkielbasa@student.agh.edu.pl>
2022-12-13 10:55:07 +0100

    VET-61-App-refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
+//pages and components
 import Home from './views/Home'
+// Routes import
+import PatientPage from './views/PatientsMainPage'
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="pages">
+      <Navbar/>
+       <div className="pages">
           <Routes>
             <Route
               path="/"
-              element={<Home />}
-            />
+              element={<Home />}/>
+              <Route path="/patient" element={<PatientPage/>}/>
           </Routes>
         </div>
-      </BrowserRouter>
+      </BrowserRouter> 
     </div>
   );
 }
