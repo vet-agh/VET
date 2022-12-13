b2c680ab1fc04c3fb1db7f46ec31028074cce247
Michał Sawicki <msawicki@student.agh.edu.pl>
2022-12-13 17:10:59 +0100

    VET-60 style changes

--- frontend/src/context/EquipmentContext.js
+++ frontend/src/context/EquipmentContext.js
@@ -5,13 +5,11 @@ export const EquipmentContext = createContext()
 export const equipmentReducer = (state, action) => {
     switch (action.type) {
         case 'SET_EQUIPMENT':
-            return {
-                equipment: action.payload
-            }
+            return {equipment: action.payload}
+        
         case 'CREATE_EQUIPMENT':
-            return {
-                equipment: [action.payload, ...state.equipment]
-            }
+            return {equipment: [action.payload, ...state.equipment]}
+        
         default:
             return state
     }
