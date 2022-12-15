35089262a39cfdf228ab7e4ffe8c28a0e8b012b2
Michał Sawicki <msawicki@student.agh.edu.pl>
2022-12-15 10:17:18 +0100

    VET-85 View for logged and not logged users

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import {Link} from 'react-router-dom'
 import {useLogout} from '../hooks/useLogout'
+import {useAuthContext} from '../hooks/useAuthContext'
 
 const Navbar = () => {
 
     const {logout} = useLogout()
+    const {user} = useAuthContext()
 
     const handleClick = () => {
         logout()
@@ -16,13 +18,18 @@ const Navbar = () => {
                 <h1>System zarządzania siecią weterynaryjną</h1>
             </Link>
             <nav>
+                {!user &&(
                 <div>
                     <Link to="/login"> Zaloguj się </Link>
                 </div>
+                )}
 
+                {user && (
                 <div>
+                    <span>{user.email}</span>
                     <button onClick={handleClick}> Wyloguj się </button>
                 </div>
+                )}
             </nav>
         </div>
     </header>
