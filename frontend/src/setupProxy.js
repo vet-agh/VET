09136f6f9402123a637ec5e8fbcabaa69a28042c
krawczk <kacperkrawczyk490@gmail.com>
2022-12-17 21:18:25 +0100

    VET-84 Fix proxy setup

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,12 +1,10 @@
-const { createProxyMiddleware} = require('http-proxy-middleware')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-
-module.exports = function(app) {
+module.exports = function(app){
   app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http:localhost:4000',
-      changeOrigin: true
-    })
-)
-}
+      createProxyMiddleware('/api', {
+          target: 'http://localhost:4000',
+          changeOrigin: true
+      })
+  )
+};
