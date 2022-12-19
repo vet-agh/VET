40ca6673ae4cd4c09218c9d12a12ef006545a75f
Michał Sawicki <msawicki@student.agh.edu.pl>
2022-12-19 17:42:55 +0100

    VET-88 correcting code

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -4,7 +4,7 @@ import {useSignup} from '../hooks/useSignup'
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {signup, error, isLoading} = useLogin()
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
