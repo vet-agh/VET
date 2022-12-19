0e36f8f526bb4c493f01107ca5059d87ace33088
Michał Sawicki <msawicki@student.agh.edu.pl>
2022-12-19 17:23:08 +0100

    VET-88 add Signup form

---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+import {useLogin} from '../hooks/useLogin'
+
+const Signup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const {signup, error, isLoading} = useLogin()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        await signup(email, password)
+    }
+
+    return (
+        <form className="signup" onSubmit={handleSubmit}> 
+            
+            <h3> Rejestracja </h3>
+
+            <label> Email:  </label>
+            <input type = "email" onChange={(e) => setEmail(e.target.value)} value={email}/> 
+
+            <label> Hasło:  </label>
+            <input type = "password" onChange={(e) => setPassword(e.target.value)} value={password}/> 
+
+            <button disabled={isLoading}> Zaloguj się </button>
+            {error && <div className="error"> {error} </div>}
+            
+        </form>
+    )
+}
+
+export default Signup
