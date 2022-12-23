70ea027a5622c2643b60a9cc930233535081bfee
krawczk <kacperkrawczyk490@gmail.com>
2022-12-23 14:12:27 +0100

    VET-93 patientModel style fix

--- backend/models/patientModel.js
+++ backend/models/patientModel.js
@@ -3,20 +3,20 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const patientSchema = new Schema({
-imie: {
-    type: String,
-    required: true
-},
-gatunek: {
-    type: String,
-    required: true
-},
-rasa: {
-    type: String,
-},
-id_klienta: {
-    type: String,
-    required: true
+    imie: {
+        type: String,
+        required: true
+    },
+    gatunek: {
+        type: String,
+        required: true
+    },
+    rasa: {
+        type: String,
+    },
+    id_klienta: {
+        type: String,
+        required: true
     }
 }, {timestamps: true})
 
