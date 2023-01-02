7832c17784560cdf4b48984c5e437db5a4216a26
maclipski <mlipski@student.agh.edu.pl>
2023-01-02 17:36:17 +0100

    VET-108 Home page update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,15 +8,16 @@ const createToken = (_id) => {
 
 // login admin
 const loginUser = async (req, res) => {
-    const {email, password} = req.body
+    const {email, password, role} = req.body
 
     try {
         const user = await User.login(email, password)
 
         // create token
         const token = createToken(user._id) 
+        const role = user.role
 
-        res.status(200).json({email, token})
+        res.status(200).json({email, token, role})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
@@ -32,7 +33,7 @@ const signupUser = async (req, res) => {
         // create token
         const token = createToken(user._id) 
 
-        res.status(200).json({email, token})
+        res.status(200).json({email, token, role})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
