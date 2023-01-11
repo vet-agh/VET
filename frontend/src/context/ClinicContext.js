83ea3a933911aa63f2ac3692c5a875082d2c05f2
Michał Sawicki <msawicki@student.agh.edu.pl>
2023-01-11 23:23:49 +0100

    VET-75 update clinics in context

--- frontend/src/context/ClinicContext.js
+++ frontend/src/context/ClinicContext.js
@@ -7,6 +7,14 @@ export const ClinicReducer = (state, action) => {
     case 'SET_CLINICS': return {clinics: action.payload}
     case 'CREATE_CLINICS': return {clinics: [action.payload, ...state.clinics]}
     case 'DELETE_CLINICS': return {clinics: state.clinics.filter((c) => c._id !== action.payload._id)}
+    case 'UPDATE_CLINICS':
+        const updatedClinics = state.clinics.map(clinic => {
+            if(clinic._id === action.payload._id) {
+                return action.payload;
+            }
+            return clinic;
+        })
+    return { clinics: updatedClinics };
     default: return state
     }
 }
